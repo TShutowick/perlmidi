4ca015bea2109736c311693f21fb3a31f1cfd8a9
tyler <>
2025-08-10 12:51:23 -0700

    add more byte constants
    and change TICKS_PER_BEAT to 24, the MIDI standard

--- lib/PerlMIDI/Constants.pm
+++ lib/PerlMIDI/Constants.pm
@@ -16,13 +16,15 @@ our @EXPORT = qw/
 /;
 
 use constant {
-	TICKS_PER_BEAT => 64,
+	TICKS_PER_BEAT => 24,
 	BYTE_CLOCK     => 0xF8, # MIDI clock byte
 	BYTE_START     => 0xFA, # MIDI start byte
 	BYTE_CONTINUE  => 0xFB, # MIDI continue byte
 	BYTE_STOP      => 0xFC, # MIDI stop byte
 	BYTE_RESET     => 0xFF, # MIDI reset byte
 	BYTE_CONTROL   => 0xB0, # MIDI control change byte (last nibble is the channel)
+	BYTE_NOTE_ON   => 0x90, # MIDI note on byte (last nibble is the channel)
+	BYTE_NOTE_OFF  => 0x80, # MIDI note off byte (last nibble is the channel)
 };
 
 1;
